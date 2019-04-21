976611b1d5b5a7fa8e81e93c193e39b30c65f6df
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-04-21 10:40:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,156 @@
+// https://www.youtube.com/watch?v=7giZGFDGnkc&list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_
+const express = require('express')
+const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const Event = require('./models/event')
+const User = require('./models/user')
+
+const app = express()
+
+// connect to mongodb cluster
+const { MONGO_USER, MANGO_PASSWORD, MONGO_DB } = process.env
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MANGO_PASSWORD}@cluster0-abeh6.mongodb.net/${MONGO_DB}?retryWrites=true`, { useNewUrlParser: true }).then(() => {
+  // app can listen from port 3000
+  app.listen(3000)
+}).catch(err => {
+  console.log(err)
+})
+
+// parse incoming json bodies
+app.use(bodyParser.json())
+
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
+
+    input UserInput {
+      email: String!
+      password: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    type RootQuery {
+      events: [Event!]!
+    }
+
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  // has all resolver functions for that schema
+  rootValue: {
+    /*
+    query {
+      events {
+        title
+        _id
+      }
+    }
+    */
+    events: () => {
+      return Event.find().then(events => {
+        console.log(events)
+        // ._doc leaves out all meta data
+        return events.map(event => ({ ...event._doc, _id: event.id }))
+      }).catch(err => {
+        console.log(err)
+        throw err
+      })
+    },
+    /*
+    mutation {
+      createEvent(eventInput: {title: "karthick", description: "THE BEST", price: 1000.0, date: "2019-04-16T18:39:03.101Z" }) {
+        _id
+        title
+        date
+      }
+    }
+    */
+    createEvent: async (args) => {
+      const creatorUserId = '5cb77fce4bf56a9636155922'
+      const event = new Event({
+        ...args.eventInput,
+        // parse from string to JS Date
+        date: new Date(args.eventInput.date),
+        creator: creatorUserId // mongoose will convert string to objectId
+      })
+      // hit DB and save it
+      try {
+        const savedEvent = await event.save()
+        // push the event to the user database (not the id)
+        const user = await User.findById(creatorUserId)
+        if (!user) {
+          // dont allow users with same user id
+          throw new Error('User Doesnt exist')
+        }
+        user.createdEvents.push(savedEvent._id)
+        user.save()
+        // return the event
+        return { ...savedEvent._doc, _id: savedEvent.id }
+      } catch(error) {
+        console.log(error)
+        throw error
+      }
+    },
+    /*
+      mutation {
+        createUser(userInput: {email: "karthick@kk.com", password: "testing" }) {
+          _id
+          email
+          password
+        }
+      }
+    */
+    createUser: async args => {
+      const previousUser = await User.findOne({ email: args.userInput.email })
+      if (previousUser) {
+        // dont allow users with same user id
+        throw new Error('User Exists Alredy')
+      } else {
+        try {
+          // 12 is a safe salt value
+          const hash = await bcrypt.hash(args.userInput.password, 12)
+          const user = new User({
+            email: args.userInput.email,
+            // always store only the hash of the password not the plain text of the password
+            // in this way even if db is ahcked one cant get the password of the user
+            password: hash
+          })
+          const savedUser = await user.save()
+          return ({ ...savedUser._doc, password: null, _id: savedUser.id })
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    }
+  },
+  graphiql: true
+}))
