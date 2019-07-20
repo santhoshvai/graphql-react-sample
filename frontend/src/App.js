fceced9c1bade39d669f07d56eea70431778e4f4
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-07-20 12:05:04 +0200

    add basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,24 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import AuthPage from './pages/Auth'
 import BookingsPage from './pages/Bookings'
 import EventsPage from './pages/Events'
+import MainNavigation from "./components/Navigation/MainNavigation"
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+        <React.Fragment>
+            <MainNavigation />
+            <main>
+                {/*switch is responsible for matching the first path and going to that */}
+                <Switch>
+                    {/*without exact, "/" will be used as a prefix, even /events will redirect to auth*/}
+                    <Redirect from="/" to="/auth" exact />
+                    <Route path="/auth" component={AuthPage} />
+                    <Route path="/events" component={EventsPage} />
+                    <Route path="/bookings" component={BookingsPage} />
+                </Switch>
+            </main>
+        </React.Fragment>
     </BrowserRouter>
   );
 }
