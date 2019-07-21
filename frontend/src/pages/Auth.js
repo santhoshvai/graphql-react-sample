9681452f080aa199dca03156d2f7684d80633f70
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-07-21 21:50:26 +0200

    add signin form - frontend only

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
+import './Auth.css'
 
 class AuthPage extends Component {
   render() {
-    return <h1>The Auth page!</h1>
+    return (
+        <form className={"auth-form"}>
+          <div className="form-control">
+            <label htmlFor={"email"}>E-Mail</label>
+            <input type={"email"} id={"email"} />
+          </div>
+          <div className="form-control">
+            <label htmlFor={"password"}>Password</label>
+            <input type={"password"} id={"password"} />
+          </div>
+          <div className={"form-actions"}>
+            <button type={"submit"}>Submit</button>
+            <button type={"button"}>Switch to signup</button>
+          </div>
+        </form>
+    )
   }
 }
 
