4737a3a08f4d0a678bac0828c4269e6535a5ceb9
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-07-27 19:19:19 +0200

    allow CORS and set other response headers

--- app.js
+++ app.js
@@ -22,9 +22,25 @@ mongoose.connect(`mongodb+srv://${MONGO_USER}:${MANGO_PASSWORD}@cluster0-abeh6.m
 // parse incoming json bodies
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  // Allow CORS -- explained here https://www.reddit.com/r/webdev/comments/5r9be9/could_someone_please_explain_cors_or_point_me_to/dd5ksrc?utm_source=share&utm_medium=web2x
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // allow only these methods, note: OPTIONS is automatically sent from browser
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
+  // the only two headers that our server can handle
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  // our server cannot specially handle options, in this case just send 200
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  // only proceed if it was not OPTIONS header
+  next()
+})
+
 // express will run isAuth on every incoming request
 app.use(isAuth)
 
+// the '/graphql is the end point localhost:8000/graphql
 app.use('/graphql', graphqlHttp({
   schema: graphqlSchema,
   // has all resolver functions for that schema
