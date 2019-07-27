b1cde17c9f1fe2af28824da91b5d267907d0f856
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-07-27 19:07:04 +0200

    make backend use a different port as frontend uses 3000 now

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const app = express()
 // connect to mongodb cluster
 const { MONGO_USER, MANGO_PASSWORD, MONGO_DB } = process.env
 mongoose.connect(`mongodb+srv://${MONGO_USER}:${MANGO_PASSWORD}@cluster0-abeh6.mongodb.net/${MONGO_DB}?retryWrites=true`, { useNewUrlParser: true }).then(() => {
-  // app can listen from port 3000
-  app.listen(3000)
+  // app can listen from port 8000, 3000 is used by front end
+  app.listen(8000)
 }).catch(err => {
   console.log(err)
 })
