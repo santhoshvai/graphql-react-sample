824e0d89714a8241ad4bd6bc12de95925e48507b
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-07-28 23:21:00 +0200

    manage context inside the root App.js and it has state to handle token stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,25 +5,50 @@ import AuthPage from './pages/Auth'
 import BookingsPage from './pages/Bookings'
 import EventsPage from './pages/Events'
 import MainNavigation from "./components/Navigation/MainNavigation"
+import AuthContext from './context/auth-context';
 
-function App() {
-  return (
-    <BrowserRouter>
-        <React.Fragment>
-            <MainNavigation />
-            <main className="main-content">
-                {/*switch is responsible for matching the first path and going to that */}
-                <Switch>
-                    {/*without exact, "/" will be used as a prefix, even /events will redirect to auth*/}
-                    <Redirect from="/" to="/auth" exact />
-                    <Route path="/auth" component={AuthPage} />
-                    <Route path="/events" component={EventsPage} />
-                    <Route path="/bookings" component={BookingsPage} />
-                </Switch>
-            </main>
-        </React.Fragment>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  state = {
+    token: null,
+    userId: null
+  }
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  }
+
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+          <React.Fragment>
+            <AuthContext.Provider
+            value={{
+                token: this.state.token,
+                userId: this.state.userId,
+                login: this.login,
+                logout: this.logout
+              }}
+            >
+              <MainNavigation />
+              <main className="main-content">
+                  {/*switch is responsible for matching the first path and going to that */}
+                  <Switch>
+                      {/*without exact, "/" will be used as a prefix, even /events will redirect to auth*/}
+                      <Redirect from="/" to="/auth" exact />
+                      <Route path="/auth" component={AuthPage} />
+                      <Route path="/events" component={EventsPage} />
+                      <Route path="/bookings" component={BookingsPage} />
+                  </Switch>
+              </main>
+            </AuthContext.Provider>
+          </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
