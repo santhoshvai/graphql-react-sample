24f88cffd14408c865a3a44f7d1d7539ca880f94
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-07-28 23:41:01 +0200

    redirect to events page just after login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,10 +38,21 @@ class App extends React.Component {
                   {/*switch is responsible for matching the first path and going to that */}
                   <Switch>
                       {/*without exact, "/" will be used as a prefix, even /events will redirect to auth*/}
-                      <Redirect from="/" to="/auth" exact />
-                      <Route path="/auth" component={AuthPage} />
+                      {/*go to auth only when not authenticated*/}
+                      {!this.state.token && <Redirect from="/" to="/auth" exact />}
+                      {/*go to events by default when authenticated*/}
+                      {this.state.token && <Redirect from="/" to="/events" exact />}
+                      {/*redirect from auth to events when authenticated -- happens after login, but when typed to browser whole page refreshes and the state is lost*/}
+                      {this.state.token && <Redirect from="/auth" to="/events" exact />}
+                      {/*show auth only when not authenticated*/}
+                      {!this.state.token && (
+                        <Route path="/auth" component={AuthPage} />
+                      )}
                       <Route path="/events" component={EventsPage} />
-                      <Route path="/bookings" component={BookingsPage} />
+                      {/*show bookings only when authenticated*/}
+                      {this.state.token && (
+                        <Route path="/bookings" component={BookingsPage} />
+                      )}
                   </Switch>
               </main>
             </AuthContext.Provider>
