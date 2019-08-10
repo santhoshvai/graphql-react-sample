5da18a0523011f2ab8f83f36987bc4d9880ccd50
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-08-10 22:11:28 +0200

    add form in the modal -- submission TODO

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -20,13 +20,39 @@ class EventsPage extends Component {
     this.setState({ creating: false })
   }
 
+  // input EventInput {
+  //   title: String!
+  //   description: String!
+  //   price: Float!
+  //   date: String!
+  // }
+
   render() {
     return (
       <React.Fragment>
         {this.state.creating && (
           <React.Fragment>
             <Backdrop />
-            <Modal title="Add Event" canCancel canConfirm onCancel={this.handleOnCancel} onConfirm={this.handleOnConfirm}><p>Modal Content</p></Modal>
+            <Modal title="Add Event" canCancel canConfirm onCancel={this.handleOnCancel} onConfirm={this.handleOnConfirm}>
+              <form onSubmit={this.submitHandler}>
+                <div className="form-control">
+                  <label htmlFor={"title"}>Title</label>
+                  <input type={"text"} id={"title"} ref={this.titleEl} />
+                </div>
+                <div className="form-control">
+                  <label htmlFor={"price"}>Price</label>
+                  <input type={"number"} id={"price"} ref={this.priceEl} />
+                </div>
+                <div className="form-control">
+                  <label htmlFor={"date"}>Date</label>
+                  <input type={"date"} id={"date"} ref={this.dateEl} />
+                </div>
+                <div className="form-control">
+                  <label htmlFor={"description"}>Description</label>
+                  <textarea id="description" rows="4" />
+                </div>
+              </form>
+            </Modal>
           </React.Fragment>
         )}
         <div className="events-control">
