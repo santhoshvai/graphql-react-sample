866477be61259835b85fe7e8627891e0e75c3225
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-08-11 20:05:42 +0200

    add more contents to the event-list-item

--- frontend/src/components/EventList/EventListItem/EventListItem.js
+++ frontend/src/components/EventList/EventListItem/EventListItem.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import './EventListItem.css'
+import AuthContext from '../../../context/auth-context'
 
-const eventListItem = props => {
-  return (
-    <li className="event__list__item">
-      {props.title}
-    </li>
-  )
+class EventListItem extends React.Component {
+  static contextType = AuthContext
+  render() {
+    const { title, price, creator } = this.props.event
+    const authUserId = this.context.userId
+    const isOwner = creator._id === authUserId
+
+    return (
+      <li className="event__list__item">
+        <div>
+          <h1>{title}</h1>
+          <h2>{`$${price}`}</h2>
+        </div>
+        <div>
+          {!isOwner && <button className={"btn"}>View Details</button>}
+          {isOwner && <p>You are the owner of this event</p>}
+        </div>
+      </li>
+    )
+  }
 }
 
-export default eventListItem
+export default EventListItem
