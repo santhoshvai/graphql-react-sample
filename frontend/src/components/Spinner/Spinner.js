0a875da57e59eba03b5baa0dc2b87467020b4e6b
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-08-12 11:42:46 +0200

    minor: added a comment to remember that the spinner component was taken from loading.io

--- frontend/src/components/Spinner/Spinner.js
+++ frontend/src/components/Spinner/Spinner.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Spinner.css'
 
+// Taken from https://loading.io/css
 const spinner = props => (
   <div className="spinner">
     <div className="lds-dual-ring"></div>
