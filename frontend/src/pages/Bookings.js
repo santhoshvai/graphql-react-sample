b059c3579f9d7518e599d6db1fc123b6a313d1ce
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-08-25 00:13:15 +0200

    add styles to booking tabs also moved it to a separate component

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -3,6 +3,7 @@ import AuthContext from '../context/auth-context';
 import Spinner from '../components/Spinner/Spinner'
 import BookingList from '../components/BookingList/BookingList'
 import BookingChart from '../components/BookingChart/BookingChart'
+import BookingTabs from '../components/BookingTabs/BookingTabs'
 
 class BookingsPage extends Component {
   static contextType = AuthContext;
@@ -113,10 +114,7 @@ class BookingsPage extends Component {
     if (!this.state.isLoading) {
       content = (
         <React.Fragment>
-          <div>
-            <button onClick={() => this.handleOnTabClick("List")}>List</button>
-            <button onClick={() => this.handleOnTabClick("Chart")}>Chart</button>
-          </div>
+          <BookingTabs activeTab={this.state.tab} onTabClick={this.handleOnTabClick} />
           <div>
             {this.state.tab === "List" ? (
               <BookingList bookings={this.state.bookings} onCancel={this.handleOnCancel} />
