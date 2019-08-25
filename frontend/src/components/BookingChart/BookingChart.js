684bf8890026cef234ef4cf5e6f1e64726fc6ed9
santhosh vaiyapuri <santhoshvai@gmail.com>
2019-08-25 22:22:39 +0200

    show price buckets in a chart

--- frontend/src/components/BookingChart/BookingChart.js
+++ frontend/src/components/BookingChart/BookingChart.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import {Bar} from 'react-chartjs-2';
+import './BookingChart.css'
+// Bar Chart Example: https://github.com/jerairrest/react-chartjs-2/blob/master/example/src/components/bar.js
 
 const BOOKING_BUCKETS = {
   cheap: {
@@ -14,9 +17,14 @@ const BOOKING_BUCKETS = {
     max: Number.MAX_VALUE
   }
 }
+const CHART_COLOR = 'rgba(81,1,209,1)'
+const CHART_COLOR_40 = 'rgba(81,1,209,0.4)'
+const CHART_COLOR_20 = 'rgba(81,1,209,0.2)'
 
 const bookingChart = props => {
   const outputCounts = {}
+  const labels = []
+  const counts = []
   for (const bucket in BOOKING_BUCKETS) {
     const { min, max } = BOOKING_BUCKETS[bucket]
     const count = props.bookings.reduce((prev, current) => {
@@ -26,10 +34,44 @@ const bookingChart = props => {
         return prev
       }
     }, 0)
+    labels.push(bucket)
+    counts.push(count)
     outputCounts[bucket] = count
   }
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Number of events booked",
+        backgroundColor: CHART_COLOR_20,
+        borderColor: CHART_COLOR,
+        borderWidth: 1,
+        hoverBackgroundColor: CHART_COLOR_40,
+        hoverBorderColor: CHART_COLOR,
+        data: counts,
+      }
+    ]
+  }
+  // if these options are not given, the y-axis seems to start from the lowest number
+  const options = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    }
+  }
   console.log(outputCounts)
-  return <p>{JSON.stringify(outputCounts)}</p>
+  return (
+    <div className="chart">
+      <Bar
+        data={data}
+        options={options}
+      />
+    </div>
+  );
 }
 
 export default bookingChart
